71c92566ebd443e8c2f7b46d893d735ef5ee224c
jsanz24 <jesus240492@gmail.com>
2019-02-14 21:53:44 +0100

    arreglando errores de David

--- routes/upejercise.js
+++ routes/upejercise.js
@@ -1,47 +1,49 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const ejerciseModel = require("../models/ejercise")
 const User = require("../models/users.js")
 
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
-router.get('/upejercise',ensureLoggedIn('/login'), (req, res, next) => {
+router.get('/upejercise', ensureLoggedIn('/login'), (req, res, next) => {
   res.render('ejercise/uptrain')
 });
 
-router.post('/add-train',ensureLoggedIn('/login'),(req,res,next)=>{
+router.post('/add-train', ensureLoggedIn('/login'), (req, res, next) => {
   let array = []
   const ejercises = req.body
 
   for (const nom in ejercises) {
-
-    
     //Al nombre le quito el numero
     let nombre = nom.replace(/[0-9]/g, '');
-    if(nombre == "aerobica"){
-// tiempo
-if(ejercises[nom][0].indexOf(":") != -1){
+    if (nombre == "aerobica") {
+      // tiempo
+      if (ejercises[nom][0].indexOf(":") != -1) {
 
-  ejercises[nom][0] = +ejercises[nom][0].replace(":",".")
-  ejercises[nom][1] = +ejercises[nom][1]
- console.log(ejercises[nom])
+        ejercises[nom][0] = +ejercises[nom][0].replace(":", ".")
+        ejercises[nom][1] = +ejercises[nom][1]
+        console.log(ejercises[nom])
+      }
     }
-  }
-    array.push({userid: req.user.id, ejercise: {[nombre]: ejercises[nom]}})
-  
+    else{
+      ejercises[nom][0] = +ejercises[nom][0]
+      ejercises[nom][1] = +ejercises[nom][1]
+    }
+    array.push({ userid: req.user.id, ejercise: { [nombre]: ejercises[nom] } })
+
   }
   ejerciseModel.insertMany(array)
-  .then((ejercises)=>{
-    const arrEj = ejercises.map(elem => elem._id)
+    .then((ejercises) => {
+      const arrEj = ejercises.map(elem => elem._id)
 
-    User.findByIdAndUpdate(req.user._id,{$push:{ejercise:{$each:arrEj}}})
-    .then(user => {
-      let message = "Entrenamiento subido con exito"
-      res.render('profile/info',{message})
+      User.findByIdAndUpdate(req.user._id, { $push: { ejercise: { $each: arrEj } } })
+        .then(user => {
+          let message = "Entrenamiento subido con exito"
+          res.render('profile/info', { message })
+        })
+        .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
-  })
-  .catch(err => console.log(err))
 })
 
 module.exports = router;
