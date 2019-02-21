a413826d38ee24f59e96d87f096daca2b54a20cf
David Casas <cem20903@gmail.com>
2019-02-21 16:46:50 +0100

    asdf

--- routes/evolution.js
+++ routes/evolution.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router  = express.Router();
 const ejerciseModel = require('../models/ejercise')
+const user = require('../models/users')
+
+router.get('/weight',(req,res,next)=>{
+
+res.render('profile/weight')
+
+})
+
+router.post('/weight',(req,res,next)=>{
+
+ 
+  req.query.weight = parseFloat(req.query.weight)
+  let id = req.query._id
+  let newWeight = req.query
+  
+  user.findByIdAndUpdate(req.user._id, { $push: { weight:  newWeight  } })
+  .then(data=>{
+  })
+
+})
+
+router.get('/weightEvo',(req,res,next)=>{
+  
+  user.findById(req.user._id)
+  .then(data=>{
+    res.json(data)
+  })
+
+})
 
 
 router.get('/info', (req,res,next)=>{
@@ -10,17 +39,21 @@ router.get('/info', (req,res,next)=>{
 
 
 
+  if(req.query.disciplina === "peso"){
+
+user.findById(req.user._id)
+.then(data=>{
+ res.json(data)
+})
+  } else {
   
   const disciplina = `ejercise.${req.query.disciplina}`
-
- // {$and: [{[disciplina]: {$exists: true}}, {date:{$gt:Date('2019-02-11 00:00:00.000')}}]}
-
   ejerciseModel.find({$and: [{[disciplina]: {$exists: true}}, {date:{$gte:from}}
 ,{date:{$lte:to}}]})
   .then(data=>{
     res.json(data)
   })
-
+}
 
 })
 
